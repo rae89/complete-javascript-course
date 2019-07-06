25c14d6f6608aa21ec8a3dadcfcf9e88aa8957ef
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-05 20:19:08 -0700

    if/else statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -88,6 +88,7 @@ console.log(typeof x);
  * Operator precedence
  */
 
+/*
 var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
@@ -114,3 +115,47 @@ x += 10;
 console.log(x);
 x++;
 console.log(x);
+*/
+
+/************
+ * If / else statements
+ */
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} 
+
+else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+ 
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married!'); 
+} 
+
+else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var massMark = 78 // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark + heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\' BMI is higher than John\'s');
+}
+else {
+    console.log('John\'s BMI is higher than Marks\'s.');
+}
+//var markHigherBMI = BMIMark > BMIJohn;
+//console.log('Is Mark\'s BMI higher than John\'s? '+ markHigherBMI);
