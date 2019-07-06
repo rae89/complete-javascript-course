80efa2b73394872c1a63916bc59bf8a2a6e7f1f2
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-05 19:58:52 -0700

    operator precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,8 @@
+/***************************************
+ * Variables and data types
+ */
+
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -13,5 +18,99 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable naming rules
 var _3years = 3;
 var johnMark = 'John and Mark';
+*/
+
+/***************************************
+ * Variable mutation and type coercion
+ */
+
+/*
+ var firstName = 'John';
+ var age = 28;
+
+ // Type coercion
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' year old '
+ + job + '. Is he married? ' + isMarried);
+
+ // Variable mutation
+ age = 'twenty eight';
+ job = 'driver';
+
+ alert(firstName + ' is a ' + age + ' year old ' + 
+ job + '. Is he married? ' + isMarried);
+
+ prompt('What is his last Name?');
+ console.log(firstName + ' ' + lastName);
+*/
+
+/***************************************
+ * Basic operators
+ */
+
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+var yearJohn = now - ageJohn;
+var yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator 
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x); 
+*/
+
+/*****
+ * Operator precedence
+ */
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge); 
+
+// Grouing
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark )/ 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// More operators 
+x *= 2;
+console.log(x)
+x += 10;
+console.log(x);
+x++;
+console.log(x);
