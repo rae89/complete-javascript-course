95406ae27081cf3ac5b3cd306a1b43bd8a93f527
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-08 23:15:59 -0700

    functions

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -236,6 +236,7 @@ switch (true) {
 }
 */
 
+/*
 var height;
 height = 23;
 if (height || height === 0) {
@@ -249,3 +250,37 @@ else {
 if (height == '23') {
     console.log('The == operator does type coercion!');
 }
+*/
+
+/******************************************
+ * Functions
+ */
+
+function calculateAge(birthYear) {
+  return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName)
+{
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' +
+        retirement + ' years. ');
+    }
+    else {
+        console.log(firstName + ' is already retired.');
+    }
+    
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
+
