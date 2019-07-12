44a5c2938f86002dc40160b95a037f350516e836
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-11 22:13:58 -0700

    loops and iteration

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -352,6 +352,7 @@ console.log(isDesigner);
  * Objects and properties
  */
 /*
+
 // Object literal 
 var john = {
     firstName: 'John',
@@ -378,6 +379,11 @@ jane[lastName] = 'Smith';
 console.log(jane);
 */
 
+/*********************************
+ * Objects and methods
+ */
+/*
+
 var john = {
     firstName: 'John',
     lastName: 'Smith',
@@ -392,10 +398,50 @@ var john = {
 
 john.calcAge();
 console.log(john);
+*/
 
+/***************************************
+ * Loops and iteration
+ */
+/*
+// For loop
+ for (var i = 0; i < 20; i+=2) {
+     console.log(i)
+ }
 
-
-
-
+ // i = 0, 0 < 10 true, log i to console, i++
+ // i = 1, 1 < 10 true, log i to the console, i++
+ // ...
+ // i = 9, 9 < 10 true, log i to the console, i++
+ // i = 10, 10 < 10 FALSE, exit the loop!
 
  
+ var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+ for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+ }
+
+ // While loop
+ var i = 0;
+ while(i < john.length) {
+     console.log(john[i]);
+     i++;
+ }
+*/
+
+// continue and break statements
+var john = ["John", "Smith", 1990, "designer", false, "blue"];
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== "string") continue;
+  console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for(var i = john.length -1; i >= 0; i--) {
+    console.log(john[i]);
+}
