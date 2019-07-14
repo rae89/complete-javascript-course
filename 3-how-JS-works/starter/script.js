617b10b93c9a89d84e3db4ba526809e73cea6f70
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-14 10:49:14 -0700

    hoisting in practice

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,7 +1,31 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+calculateAge(1965);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+// retirement(1956);
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
+
+// variables
+
+console.log(age);
+var age = 23;
+console.log(age);
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
