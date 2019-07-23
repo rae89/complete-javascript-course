dfa17f96515f6bac871b0dfd164823f77e2c3173
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-22 22:08:46 -0700

    primitives vs objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -34,6 +34,7 @@ console.log(mark.lastName);
 */
 
 // Object.create
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -50,3 +51,42 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 });
+*/
+
+// Primitives vs objects
+
+//Primitives 
+var a = 23
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
