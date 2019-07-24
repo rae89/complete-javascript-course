7793c0987629c36ab5372de00bfaf951be0fccf2
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-23 21:52:07 -0700

    first class functions: passing functions as arguments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -55,6 +55,7 @@ var jane = Object.create(personProto, {
 
 // Primitives vs objects
 
+/*
 //Primitives 
 var a = 23
 var b = a;
@@ -90,3 +91,39 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
+
+// Passing functions as arguments
+
+var years = [1990, 1965, 1997, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
