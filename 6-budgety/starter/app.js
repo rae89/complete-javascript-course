b6523027a455dc5afce0712fc955831c141c862d
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-08-03 10:19:20 -0700

    reading input data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -3,23 +3,45 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-  // Some code
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-  var ctrlAddItem = function() {
+    var DOM = UICtrl.getDOMstrings();
+    var ctrlAddItem = function() {
     // 1. Get the filed input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
     console.log("It works");
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
-    }
+    } 
   });
 })(budgetController, UIController);
