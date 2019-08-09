ab4976a1f1aac59a859f3e2b4dd8e5a71bb6e795
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-08-08 18:01:27 -0700

    doh

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -144,20 +144,17 @@ var controller = (function(budgetCtrl, UICtrl) {
     input = UICtrl.getInput();
 
     if (input.description !== "" && isNaN(input.value) && input.value > 0) {
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+      // 4. Clear the fields
+      UICtrl.clearFields();
 
-        // 5. Calculate and update budget
-        updateBudget();
+      // 5. Calculate and update budget
+      updateBudget();
     }
-
-    
-    
   };
 
   return {
