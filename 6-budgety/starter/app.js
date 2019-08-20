d4c5791b2952f672705788bcd2d21472d98adb41
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-08-19 18:39:51 -0700

    deleting an item from the UI

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -161,6 +161,12 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    deleteListItem: function(selectorID) {
+
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el)
+    },
+
     clearFields: function() {
       var fields, fieldsArr;
       fields = document.querySelectorAll(
@@ -252,10 +258,10 @@ var controller = (function(budgetCtrl, UICtrl) {
       budgetCtrl.deleteItem(type, ID);
 
       // 2. delete the item from the UI
-
+      UICtrl.deleteListItem(itemID);
 
       // 3. update and show the new budget
-
+      updateBudget();
     }
   }
 
