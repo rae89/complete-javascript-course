2a65bd1af0b6580229632b4f0abe703a1b866eac
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-10-12 12:24:11 -0700

    display the current month and year

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -153,7 +153,8 @@ var UIController = (function() {
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
     container: ".container",
-    expensesPercLabel: ".item__percentage"
+    expensesPercLabel: ".item__percentage",
+    dateLabel: '.budget__title--month'
   };
 
   var formatNumber = function(num, type) {
@@ -267,6 +268,19 @@ var UIController = (function() {
       });
     },
 
+    displayMonth: function() {
+      var now, months, month, year;
+
+      var now = new Date();
+      //var christmas = new Date(2016, 11, 25);
+      months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August','September','October', 'November', 'December'];
+      month = now.getMonth();
+
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' +year;
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -356,6 +370,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log("Application has started.");
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
