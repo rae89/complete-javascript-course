7dbb50659f4850c8c28fa34ad4f581ead6e6885d
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-10-27 21:38:44 -0700

    destructuring

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -191,7 +191,7 @@ const box6 = {
 box6.clickMe();
 */
 
-
+/*
 function Person(name) {
   this.name = name;
 }
@@ -217,3 +217,41 @@ Person.prototype.myFriends5 =
     console.log(arr);
   }
   new Person('Mike').myFriends6(friends);
+*/
+
+/////////////////////////////////////////////////
+// Lecture: Destructuring 
+
+// ES5
+var john = ['John', 26];
+var name5 = john[0];
+var age5 = john[1];
+console.log(name5); 
+console.log(age5);
+
+// ES6 
+const [name6, age6] = ['John', 26];
+console.log(name6);
+console.log(age6);
+
+const obj = {
+  firstName: 'John',
+  lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName:a , lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+  const age = new Date().getFullYear() - year;
+  return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1999);
+console.log(age2);
+console.log(retirement);
