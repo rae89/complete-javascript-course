27aad32e49390d65f66716c4dd2e215c28d20355
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-10-27 18:55:41 -0700

    arrow functions: basics

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -80,7 +80,7 @@ console.log(c);
 
 //////////////////////////////////
 // Lecture: Strings
-
+/*
 let firstName = "John";
 let lastName = "Smith";
 const yearOfBirth = 1990;
@@ -115,3 +115,29 @@ console.log(n.startsWith('j'));
 console.log(n.endsWith('Sm'));
 console.log(n.includes('oh'));
 console.log(firstName.repeat(5));
+*/
+
+//////////////////////////////////////////////////
+// Lecture: Arror functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+  return 2016 - el;
+});
+console.log(ages5);
+
+// ES6   
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
