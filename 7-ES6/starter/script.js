d82df6604eca61f1ce12a638472709e24875d10f
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-10-27 19:19:17 -0700

    arrow functions: lexical this keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -117,6 +117,7 @@ console.log(n.includes('oh'));
 console.log(firstName.repeat(5));
 */
 
+/*
 //////////////////////////////////////////////////
 // Lecture: Arror functions
 
@@ -141,3 +142,78 @@ ages6 = years.map((el, index) => {
   return `Age element ${index + 1}: ${age}.`
 });
 console.log(ages6);
+*/
+
+/////////////////////////////////////////////////////
+// Lecture: Arror functions 2
+
+// ES5 
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector('.green').addEventListener('click', function() {
+      var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+      alert(str);
+    });
+  }
+}
+//box5.clickMe();
+
+// ES6
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    });
+  }
+}
+//box6.clickMe();
+
+// this example wont work since the outter arrow function does not have a this keyword defined.
+/*
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: () => {
+
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    });
+  }
+}
+box6.clickMe();
+*/
+
+
+function Person(name) {
+  this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = 
+  function(friends) {
+    var arr = friends.map(function(el)
+    {
+      return this.name + ' is friends with ' + el;
+    }.bind(this));
+    console.log(arr);
+  }
+  var friends = ['Bob', 'Jane', 'Mark'];
+  new Person('John').myFriends5(friends);
+
+
+  // ES6 
+  Person.prototype.myFriends6 = 
+  function(friends) {
+    var arr = friends.map(el =>
+    `${this.name} is friends with ${el}`);
+    console.log(arr);
+  }
+  new Person('Mike').myFriends6(friends);
