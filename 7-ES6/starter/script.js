6d596d1a2ea349a84c7fa1899caa95004e0c3deb
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-10-28 22:25:22 -0700

    the spread operator

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -260,6 +260,7 @@ console.log(retirement);
 
 //////////////////////////////////////////////////////////
 // Lecture: Arrays
+/*
 const boxes = document.querySelectorAll('.box');
 
 // ES5 
@@ -271,12 +272,13 @@ boxesArr5.forEach(function(cur) {
 
 
 // ES6 
+
 const boxesArr6 = Array.from(boxes);
 Array.from(boxes).forEach(cur => cur.getElementsByClassName.backgroundColor = 'dodgerblue');
 
 
 // ES5
-/*
+
 for( var i = 0; i < boxesArr5.length; i++) {
   if(boxesArr5[i].className === 'box blue') {
     continue;
@@ -284,7 +286,7 @@ for( var i = 0; i < boxesArr5.length; i++) {
 
   boxesArr5[i].textContent = 'I Changed to blue'
 }
-*/
+
 
 //ES6 
 for (const cur of boxesArr6) {
@@ -307,5 +309,35 @@ console.log(ages[full.indexOf(true)]);
 //ES6 
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
 
 
+/////////////////////////////////////////////////////////
+// Lecture: Spread operator
+
+function addFourAges (a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+//ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+//
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
